6923216c1e8904ee6119a9e24d13ae9897771100
Cale Tilford <combinekidd@gmail.com>
2016-06-29 17:08:51 +0100

    Add Unboxed News to feed

--- lib/tasks/collect.rake
+++ lib/tasks/collect.rake
@@ -2,4 +2,5 @@ desc "Collect Unboxed feeds"
 task :collect_feeds => :environment do
 	UnboxedBlog.content_items
 	UnboxedTwitter.content_items
+	UnboxedNews.content_items
 end
---
+++ lib/unboxed_news.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+module UnboxedNews
+	def self.content_items
+		doc = Nokogiri::XML(open("https://unboxed.co/news/feed.xml"))
+		doc.collect_namespaces
+		doc.remove_namespaces!
+
+		doc.xpath('//entry').each do |node|
+			title = node.xpath('title').text
+			link = node.xpath('link/@href').text
+			time = DateTime.parse(node.xpath('published').text)
+			ContentItem.where(url: link).first_or_create(url: link, title: title, published_at: time, category: 'news')
+		end
+	end
+end
