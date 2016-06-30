22d79d31bcdf8dbb21d4f9c7cda73eafffba883f
Cale Tilford <combinekidd@gmail.com>
2016-06-30 13:18:19 +0100

    wip

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -1,5 +1,5 @@
 class ContentItemsController < ApplicationController
 	def index
-		@content_items = ContentItem.where(category: ['blog', 'news']).order(published_at: :desc)
+		@content_items = ContentItem.order(published_at: :desc)
 	end
 end
