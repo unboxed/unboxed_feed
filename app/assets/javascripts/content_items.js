e66a6504b6ad3a907d6221b8a923359f50c851c5
Cale Tilford <combinekidd@gmail.com>
2016-07-21 11:43:32 +0100

    Add sticky header

--- app/assets/javascripts/content_items.js
+++ app/assets/javascripts/content_items.js
@@ -9,4 +9,17 @@ $(document).ready(function() {
 			$("#" + this.id + ".item-image").fadeIn();
 		});
 	});
+
+	var nav = $('.header__nav');
+	var padding = $('.header__padding'); 
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 300) {  
+	    	nav.addClass("header__nav--sticky");
+	    	padding.addClass("header__padding--display-true");
+	  	} else {
+	    	nav.removeClass("header__nav--sticky");
+	    	padding.removeClass("header__padding--display-true");
+	  	}
+	});
 });
