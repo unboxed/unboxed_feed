1f5a1c38629bf4e0647d7e4bb0133aed55f5637a
Cale Tilford <combinekidd@gmail.com>
2016-07-21 15:43:41 +0100

    Fade in icons on scroll

--- app/assets/javascripts/content_items.js
+++ app/assets/javascripts/content_items.js
@@ -14,12 +14,14 @@ $(document).ready(function() {
 	var padding = $('.header__padding'); 
 
 	$(window).scroll(function() {
-		if ($(this).scrollTop() > 300) {  
+		if ($(this).scrollTop() > 380) {  
 	    	nav.addClass("header__nav--sticky");
 	    	padding.addClass("header__padding--display-true");
+	    	$(".header__nav-icon").fadeIn();
 	  	} else {
 	    	nav.removeClass("header__nav--sticky");
 	    	padding.removeClass("header__padding--display-true");
+	    	$(".header__nav-icon").fadeOut();
 	  	}
 	});
 
