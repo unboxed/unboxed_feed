a6f56bb0aa065115c4d911885abfc31b9de772a6
Cale Tilford <combinekidd@gmail.com>
2016-07-22 16:19:40 +0100

    yet another WIP

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -2,6 +2,9 @@ class ContentItemsController < ApplicationController
 	def index
 		if params[:tags]
 			@content_items = ContentItem.tagged_with(params[:tags], :any => true).order(published_at: :desc)
+		elsif params[:temp]
+			@content_items = [];
+			params[:tags] = [];
 		else
 			@content_items = ContentItem.order(published_at: :desc)
 		end
