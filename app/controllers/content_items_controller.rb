40567c0de66ae7a132607f859110cb9800c2af96
Cale Tilford <combinekidd@gmail.com>
2016-07-22 12:25:06 +0100

    wip

--- app/controllers/content_items_controller.rb
+++ app/controllers/content_items_controller.rb
@@ -1,6 +1,7 @@
 class ContentItemsController < ApplicationController
 	def index
-		@content_items = ContentItem.order(published_at: :desc)
+		# binding.pry
+		@content_items = ContentItem.tagged_with(["twitter"]).order(published_at: :desc)
 		@quote = Quote.order("RANDOM()").first
 	end
 end
